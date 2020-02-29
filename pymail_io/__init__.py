6bc58de3c1bdeece2f3de1d51f166b2b22aab1b3
Joe Gasewicz <joegasewicz@gmail.com>
2020-02-29 00:32:05 +0000

    readme

--- pymail_io/__init__.py
+++ pymail_io/__init__.py
@@ -1,2 +1,8 @@
-class PyMailIO:
-    pass
+"""
+PymailIO
+ - SMTP: https://tools.ietf.org/html/rfc821.html
+ - ESMTP Service extensions: https://tools.ietf.org/html/rfc1869.html
+
+Start an SMTP connection that is secured from the beginning using SMTP_SSL().
+Start an unsecured SMTP connection that can then be encrypted using .starttls().
+"""
---
+++ pymail_io/pymail_io.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import smtplib
+from email.message import EmailMessage
+
+
+email_msg = EmailMessage()
+
+email_msg["Subject"] = "test here"
+email_msg["From"] = "joegoosebass@gmail.com"
+email_msg["To"] = "joegoosebass@gmail.com"
+
+s = smtplib.SMTP("localhost")
+s.send_message(email_msg)
+s.quit()
+
+
+class AbstractConnection(ABC):
+    pass
+
+
+
+class PyMailIO:
+
+    def __init__(self):
+        pass
+
+    def send(self):
+
+
