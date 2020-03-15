92fd6a342357b80b3f132454a9361581ca1896b5
Joe Gasewicz <joegasewicz@gmail.com>
2020-03-15 20:53:56 +0000

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pymail-io",
-    version="0.0.2",
+    version="0.0.4",
     description="An asynchronous mail server",
     packages=['pymail_io'],
     install_requires=[
